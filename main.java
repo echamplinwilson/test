774158892ec4a2f96c525ccd02ad38f25b53a200
Ellie Wilson <eleanorelisabethwilson@gmail.com>
2023-01-11 20:07:32 -0800

    did stuff

---
+++ main.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!"); 
+        System.out.print("Hi Ellie");
+        System.out.print("Stuff\n");
+    }
+}
